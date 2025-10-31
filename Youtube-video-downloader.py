a6ea9c96a8be67a0f403bf1d021a3ad996fe40fb
Swagata Adhya <103314170+swagataadhya@users.noreply.github.com>
2025-10-31 15:34:47 +0530

    Final commit Youtube-video-downloader

---
+++ Youtube-video-downloader.py
@@ -0,0 +1,41 @@
+from pytubefix import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        print(f"Downloading: {yt.title} ...")
+        highest_res_stream.download(output_path=save_path)
+        print("✅ Video downloaded successfully!")
+    except Exception as e:
+        import traceback
+        print("❌ Error during download:")
+        traceback.print_exc()
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
+    return folder
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    video_url = input("Please enter a YouTube url: ").strip()
+
+    if "?si=" in video_url:
+        video_url = video_url.split("?si=")[0]
+    if "&" in video_url:
+        video_url = video_url.split("&")[0]
+
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("Started download...")
+        download_video(video_url, save_dir)
+    else:
+        print("Invalid save location.")
